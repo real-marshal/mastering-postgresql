36271f609cdd68d27ec31e54c70cdc270b8dc8b3
Real_Marshal <remarshal@protonmail.com>
2022-12-16 22:58:47 +0300

    fix formatting

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
 -- top drivers of all time
 select format('%s %s', forename, surname) as name, count(*) as wins
 from drivers
-         left join results using (driverid)
-         left join races using (raceid)
+    left join results using (driverid)
+    left join races using (raceid)
 where position = 1
 group by driverid
 order by wins desc;
@@ -17,30 +17,30 @@ with decades as (
 select decade, name, wins,
        rank() over (partition by decade order by wins desc)
 from decades
-         left join lateral (
-    select format('%s %s', forename, surname) as name, count(*) as wins
-    from drivers
+    left join lateral (
+        select format('%s %s', forename, surname) as name, count(*) as wins
+        from drivers
              left join results using (driverid)
              left join races using (raceid)
-    where position = 1 and year >= decade and year < decade + 10
-    group by decade, driverid
-    order by wins desc
-    limit 3
+        where position = 1 and year >= decade and year < decade + 10
+        group by decade, driverid
+        order by wins desc
+        limit 3
     ) as winners on true
 order by decade, wins desc;
 
 -- drivers who never finished a race
 select name, races
 from (
-         select format('%s %s', forename, surname) as name,
-                every(position is null) as never_finished,
-                count(raceid) as races
-         from drivers
-                  left join results using(driverid)
-                  left join races using(raceid)
-         group by driverid
-         order by races desc
-     ) as idk
+    select format('%s %s', forename, surname) as name,
+        every(position is null) as never_finished,
+        count(raceid) as races
+    from drivers
+        left join results using(driverid)
+        left join races using(raceid)
+    group by driverid
+    order by races desc
+) as idk
 where never_finished = true;
 
 -- reasons for never completing a race of the guy with
@@ -51,8 +51,8 @@ with
                every(position is null) as never_finished,
                count(raceid) as races
         from drivers
-                 left join results using(driverid)
-                 left join races using(raceid)
+            left join results using(driverid)
+            left join races using(raceid)
         group by driverid
         order by races desc
     ),
@@ -65,9 +65,9 @@ with
     )
 select races.name, status, races.date
 from results
-         left join status using(statusid)
-         left join drivers using(driverid)
-         left join races using(raceid)
+    left join status using(statusid)
+    left join drivers using(driverid)
+    left join races using(raceid)
 where format('%s %s', forename, surname) =
-      (select name from top_driver_who_never_finished)
+    (select name from top_driver_who_never_finished)
 order by races.date;
